aed3e08a8d7783815131ba9da5bed88bf5cc1849
Andre N. Klingsheim <klingsen@gmail.com>
2012-08-19 23:09:36 +0200

    Fixed version numbers, fixed example config. This is version 1.0.1 :)

--- NWebsec/build.bat
+++ NWebsec/build.bat
@@ -5,5 +5,5 @@ msbuild NWebsec.csproj /tv:3.5 /p:Configuration=Release /p:TargetFrameworkVersio
 msbuild NWebsec.csproj /tv:4.0 /p:Configuration=Release /p:TargetFrameworkVersion=v4.0 /p:OutputPath="d:\nuget\nwebsec\lib\40" /t:Rebuild
 
 @cp NWebsec.nuspec d:\nuget\nwebsec\
-
+@cp web.config.transform d:\nuget\nwebsec\content\
 @echo "Now go to d:\nuget\nwebsec\, update nuspec version number, and publish! :)"
