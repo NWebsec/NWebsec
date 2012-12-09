c72f566895622991128d92b9ac383c91e3d13f4a
klings <klingsen@gmail.com>
2012-12-09 18:57:07 +0100

    Fixed license in all files. Quick cleanup to make all files resharper green.

--- NWebsec.Mvc/HttpHeaders/XContentSecurityPolicyAttribute.cs
+++ NWebsec.Mvc/HttpHeaders/XContentSecurityPolicyAttribute.cs
@@ -1,35 +1,7 @@
-﻿#region License
-/*
-Copyright (c) 2012, André N. Klingsheim
-All rights reserved.
-
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-
-* Redistributions of source code must retain the above copyright notice, this
-  list of conditions and the following disclaimer.
-
-* Redistributions in binary form must reproduce the above copyright notice,
-  this list of conditions and the following disclaimer in the documentation
-  and/or other materials provided with the distribution.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
-IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
-INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
-OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
-OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
-#endregion
+﻿// Copyright (c) André N. Klingsheim. See License.txt in the project root for license information.
 
 using System;
-using System.Diagnostics;
 using System.Web.Mvc;
-using NWebsec.HttpHeaders;
 
 namespace NWebsec.Mvc.HttpHeaders
 {
@@ -37,21 +9,8 @@ namespace NWebsec.Mvc.HttpHeaders
     [Obsolete("This attribute has been discontinued. Please use the new CSP attributes in the NWebsec.Mvc.HttpHeaders.Csp namespace", true)]
     public class XContentSecurityPolicyAttribute : ActionFilterAttribute
     {
-        private string directiveName;
-        private string sourceList;
-
         public XContentSecurityPolicyAttribute(String directive, string sources)
         {
-            directiveName = directive;
-            sourceList = sources;
-
-        }
-
-        public override void OnActionExecuting(ActionExecutingContext filterContext)
-        {
-            //new HttpHeaderHelper(filterContext.HttpContext).SetContentSecurityPolicyDirectiveOverride(directiveName,
-            //                                                                                          sourceList, false);
-            base.OnActionExecuting(filterContext);
         }
     }
 }
