9d1250db19987672eb4b6a97bbb3d5ba34f312f2
klings <klingsen@gmail.com>
2013-03-28 16:01:52 +0100

    Updated nuspec, some minor testing in a demo site [23]

---
+++ Source/DemoSiteWebForms/Redirect.aspx.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DemoSiteWebForms
+{
+    public partial class Redirect : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Response.Redirect("http://localhost/NWebsecWebforms/Default.aspx", true);
+        }
+    }
+}
