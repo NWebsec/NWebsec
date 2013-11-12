c4ac062e6d7ab6ef5685361ed5b7043d2f5c974b
klings <klingsen@gmail.com>
2013-11-13 00:52:29 +0100

    Added missing area

---
+++ NWebsecTestSites/Mvc4/Areas/WithConfig/Controllers/CspReportOnlyController.cs
@@ -0,0 +1,39 @@
+﻿// Copyright (c) André N. Klingsheim. See License.txt in the project root for license information.
+
+using System.Web.Mvc;
+using NWebsec.Csp;
+using NWebsec.Mvc.HttpHeaders.Csp;
+
+namespace Mvc4.Areas.WithConfig.Controllers
+{
+    [CspReportOnly]
+    [CspDefaultSrcReportOnly(Self = Source.Enable)]
+    public class CspReportOnlyController : Controller
+    {
+        //
+        // GET: /CspReportOnly/
+
+        public ActionResult Index()
+        {
+            return View("Index");
+        }
+
+        [CspReportOnly(Enabled = false)]
+        public ActionResult Disabled()
+        {
+            return View("Index");
+        }
+
+        [CspReportOnly(XContentSecurityPolicyHeader = true)]
+        public ActionResult XCsp()
+        {
+            return View("Index");
+        }
+
+        [CspReportOnly(XWebKitCspHeader = true)]
+        public ActionResult XWebKitCsp()
+        {
+            return View("Index");
+        }
+    }
+}
---
+++ NWebsecTestSites/Mvc4/Areas/WithConfig/Controllers/NoCacheHeadersController.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) André N. Klingsheim. See License.txt in the project root for license information.
+
+using System.Web.Mvc;
+using NWebsec.Mvc.HttpHeaders;
+
+namespace Mvc4.Areas.WithConfig.Controllers
+{
+    public class NoCacheHeadersController : Controller
+    {
+        //
+        // GET: /SetNoCacheHeaders/
+
+        public ActionResult Index()
+        {
+            return View("Index");
+        }
+
+        [SetNoCacheHttpHeaders]
+        public ActionResult ConfigAndAttribute()
+        {
+            return View("Index");
+        }
+
+        [SetNoCacheHttpHeaders(Enabled = false)]
+        public ActionResult Disabled()
+        {
+            return View("Index");
+        }
+    }
+}
