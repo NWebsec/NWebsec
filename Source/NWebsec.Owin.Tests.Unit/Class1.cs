daaac2a61628ed29e48331ca5269518849ced73c
klings <klingsen@gmail.com>
2014-04-16 00:42:07 +0200

    Commented out test class.

--- Source/NWebsec.Owin.Tests.Unit/Class1.cs
+++ Source/NWebsec.Owin.Tests.Unit/Class1.cs
@@ -1,41 +1,37 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
-using System.Text;
-using System.Threading.Tasks;
 using Microsoft.Owin.Testing;
 using NUnit.Framework;
-using NWebsec.HttpHeaders;
 using Owin;
+using NWebsec.Owin;
 
 namespace NWebsec.Owin.Tests.Unit
 {
     //[TestFixture]
-    public class Class1
-    {
+    //public class Class1
+    //{
 
-        //[Test]
-        public async void OwinAppTest()
-        {
-            using (var server = TestServer.Create(app =>
-            {
-                //app.UseErrorPage(); // See Microsoft.Owin.Diagnostics
-                app.UseWelcomePage("/Welcome"); // See Microsoft.Owin.Diagnostics
-                app.UseXfo(XFrameOptionsPolicy.Deny);
-                app.Run(async context =>
-                {
-                    await context.Response.WriteAsync("Hello world using OWIN TestServer");
-                });
-            }))
-            {
-                HttpResponseMessage response = await server.CreateRequest("/").AddHeader("header1", "headervalue1").GetAsync();
+    //    //[Test]
+    //    public async void OwinAppTest()
+    //    {
+    //        using (var server = TestServer.Create(app =>
+    //        {
+    //            //app.UseErrorPage(); // See Microsoft.Owin.Diagnostics
+    //            app.UseWelcomePage("/Welcome"); // See Microsoft.Owin.Diagnostics
+    //            app.UseXfo(options => options.Deny());
+    //            app.Run(async context =>
+    //            {
+    //                await context.Response.WriteAsync("Hello world using OWIN TestServer");
+    //            });
+    //        }))
+    //        {
+    //            HttpResponseMessage response = await server.CreateRequest("/").AddHeader("header1", "headervalue1").GetAsync();
 
-                var r = await response.Content.ReadAsStringAsync();
-                Console.WriteLine(r);
-                //Execute necessary tests
-                Assert.AreEqual("Hello world using OWIN TestServer", r);
-            }
-        }
-    }
+    //            var r = await response.Content.ReadAsStringAsync();
+    //            Console.WriteLine(r);
+    //            //Execute necessary tests
+    //            Assert.AreEqual("Hello world using OWIN TestServer", r);
+    //        }
+    //    }
+    //}
 }
