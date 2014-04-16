059ab5ca913d55d88d83ec40be606bee91b5e7b6
klings <klingsen@gmail.com>
2014-04-16 12:13:09 +0200

    Updated NuGet packages, renamed test project.

---
+++ NWebsecTestSites/NWebsec.Tests.Integration/TestHelper.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) André N. Klingsheim. See License.txt in the project root for license information.
+
+using System;
+
+namespace NWebsec.Tests.Functional
+{
+    public class TestHelper
+    {
+        internal Uri GetUri(string baseUri, string path, string query="")
+        {
+            var absoluteUri = new UriBuilder(baseUri);
+            absoluteUri.Path += path;
+            absoluteUri.Query = query;
+            return absoluteUri.Uri;
+        }
+    }
+}
