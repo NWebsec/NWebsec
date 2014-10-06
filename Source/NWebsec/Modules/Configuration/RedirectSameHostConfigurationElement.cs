94ba4b1b9d92d2e18920b8e0f3d52225fa6a9663
klings <klingsen@gmail.com>
2014-10-06 12:03:31 +0200

    Added missing file.
    Related Work Items: #31

---
+++ Source/NWebsec/Modules/Configuration/RedirectSameHostConfigurationElement.cs
@@ -0,0 +1,53 @@
+// Copyright (c) André N. Klingsheim. See License.txt in the project root for license information.
+
+using System;
+using System.Configuration;
+using System.Linq;
+using NWebsec.Core.HttpHeaders.Configuration;
+
+namespace NWebsec.Modules.Configuration
+{
+    public class RedirectSameHostConfigurationElement : ConfigurationElement, ISameHostHttpsRedirectConfiguration
+    {
+        private int[] _httpsPorts;
+
+        [ConfigurationProperty("enabled", IsRequired = true, DefaultValue = false)]
+        public bool Enabled
+        {
+            get
+            {
+                return (bool)this["enabled"];
+            }
+            set
+            {
+                this["enabled"] = value;
+            }
+        }
+
+        [ConfigurationProperty("httpsPorts", IsRequired = false, DefaultValue = "")]
+        public string HttpsPorts
+        {
+            get
+            {
+                return (string)this["httpsPorts"];
+            }
+            set
+            {
+                this["httpsPorts"] = value;
+            }
+        }
+
+        public int[] Ports
+        {
+            get
+            {
+                if (String.IsNullOrEmpty(HttpsPorts.Trim()))
+                {
+                    return _httpsPorts = new int[0];
+                }
+                return _httpsPorts ?? (_httpsPorts = HttpsPorts.Split(new[] { ',' }).Select(c => Int32.Parse(c.Trim())).ToArray());
+            }
+            set { throw new NotImplementedException(); }
+        }
+    }
+}
