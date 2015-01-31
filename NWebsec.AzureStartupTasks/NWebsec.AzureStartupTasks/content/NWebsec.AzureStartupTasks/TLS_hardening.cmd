b7d0fe578963a97a9ad65c34645bdb31373aa736
klings <klingsen@gmail.com>
2015-01-31 18:35:57 +0100

    Deleted startup tasks. They now live in their own repo.

--- NWebsec.AzureStartupTasks/NWebsec.AzureStartupTasks/content/NWebsec.AzureStartupTasks/TLS_hardening.cmd
+++
@@ -1,9 +0,0 @@
-REM Copyright (c) André N. Klingsheim. See https://nwebsec.codeplex.com/license for license information.
-
-IF "%NWebsecInComputeEmulator%" == "false" (
-    pushd %~dp0
-    PowerShell -ExecutionPolicy Unrestricted scripts\TLS_hardening.ps1 -AllowReboot 1 >> "%TEMP%\NWebsec.AzureStartupTasksLog.txt" 2>&1
-	popd
-)
-
-EXIT /B %errorlevel%
--- NWebsec.AzureStartupTasks/NWebsec.AzureStartupTasks/tools/Install.ps1
+++
@@ -1,4 +0,0 @@
-param($installPath, $toolsPath, $package, $project)
-$project.ProjectItems.Item("NWebsec.AzureStartupTasks").ProjectItems.Item("ReadMe.txt").Properties.Item("BuildAction").Value = 0
-$project.ProjectItems.Item("NWebsec.AzureStartupTasks").ProjectItems.Item("TLS_hardening.cmd").Properties.Item("CopyToOutputDirectory").Value = 1
-$project.ProjectItems.Item("NWebsec.AzureStartupTasks").ProjectItems.Item("scripts").ProjectItems.Item("TLS_hardening.ps1").Properties.Item("CopyToOutputDirectory").Value = 1
