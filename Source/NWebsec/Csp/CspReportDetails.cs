eb27bbd3f7c3ad69059e96fee0166fb551b9a07e
klings <klingsen@gmail.com>
2015-09-27 00:36:50 +0200

    Rolled back changes to CspReportDetails. Thank god for unit tests. #60

--- Source/NWebsec/Csp/CspReportDetails.cs
+++ Source/NWebsec/Csp/CspReportDetails.cs
@@ -12,71 +12,127 @@ namespace NWebsec.Csp
     [DataContract]
     public class CspReportDetails
     {
-        
+        //We need variables with null check when returning values from the properties. The Json deserializer will set a bunch of these to null...
+        private string _documentUri;
+        private string _referrer;
+        private string _blockedUri;
+        private string _violatedDirective;
+        private string _originalPolicy;
+        private string _effectiveDirective;
+        private string _statusCode;
+        private string _sourceFile;
+        private string _lineNumber;
+        private string _columnNumber;
+        private string _scriptSample;
+
         /// <summary>
         /// Gets the URI of the resource that was prevented from loading due to the policy violation. The default is the empty string.
         /// </summary>
         [DataMember(Name = "blocked-uri")]
-        public string BlockedUri { get; set; } = String.Empty;
-        
+        public string BlockedUri
+        {
+            get { return _blockedUri ?? String.Empty; }
+            set { _blockedUri = value; }
+        }
+
         /// <summary>
         /// Gets the address of the protected resource where the policy was violated. The default is the empty string.
         /// </summary>
         [DataMember(Name = "document-uri")]
-        public string DocumentUri { get; set; } = String.Empty;
+        public string DocumentUri
+        {
+            get { return _documentUri ?? String.Empty; }
+            set { _documentUri = value; }
+        }
 
         /// <summary>
         /// Gets the CSP directive that was violated. The default is the empty string.
         /// </summary>
         [DataMember(Name = "effective-directive")]
-        public string EffectiveDirective { get; set; } = String.Empty;
+        public string EffectiveDirective
+        {
+            get { return _effectiveDirective ?? String.Empty; }
+            set { _effectiveDirective = value; }
+        }
 
         /// <summary>
         /// Gets the original policy as received by the user-agent. The default is the empty string.
         /// </summary>
         [DataMember(Name = "original-policy")]
-        public string OriginalPolicy { get; set; } = String.Empty;
+        public string OriginalPolicy
+        {
+            get { return _originalPolicy ?? String.Empty; }
+            set { _originalPolicy = value; }
+        }
 
         /// <summary>
         /// Gets the referrer attribute of the protected resource. The default is the empty string.
         /// </summary>
         [DataMember(Name = "referrer")]
-        public string Referrer { get; set; } = String.Empty;
+        public string Referrer
+        {
+            get { return _referrer ?? String.Empty; }
+            set { _referrer = value; }
+        }
 
         /// <summary>
         /// Gets the status code of the HTTP response that contained the protected resource. The default is the empty string.
         /// </summary>
         [DataMember(Name = "status-code")]
-        public string StatusCode { get; set; } = String.Empty;
+        public string StatusCode
+        {
+            get { return _statusCode ?? String.Empty; }
+            set { _statusCode = value; }
+        }
 
         /// <summary>
         /// Gets the policy directive that was violated, as it appears in the policy. The default is the empty string.
         /// </summary>
         [DataMember(Name = "violated-directive")]
-        public string ViolatedDirective { get; set; } = String.Empty;
+        public string ViolatedDirective
+        {
+            get { return _violatedDirective ?? String.Empty; }
+            set { _violatedDirective = value; }
+        }
 
         /// <summary>
         /// Gets the URI of the resource where the violation occurred, stripped for reporting.
         /// </summary>
         [DataMember(Name = "source-file")]
-        public string SourceFile { get; set; } = String.Empty;
+        public string SourceFile
+        {
+            get { return _sourceFile ?? String.Empty; }
+            set { _sourceFile = value; }
+        }
 
         /// <summary>
         /// Gets the line number in the source file on which the violation occurred. The default is the empty string.
         /// </summary>
         [DataMember(Name = "line-number")]
-        public string LineNumber { get; set; } = String.Empty;
+        public string LineNumber
+        {
+            get { return _lineNumber ?? String.Empty; }
+            set { _lineNumber = value; }
+        }
 
         /// <summary>
         /// Gets the column number in the source file on which the violation occurred. The default is the empty string.
         /// </summary>
         [DataMember(Name = "column-number")]
-        public string ColumnNumber { get; set; } = String.Empty;
+        public string ColumnNumber
+        {
+            get { return _columnNumber ?? String.Empty; }
+            set { _columnNumber = value; }
+        }
 
         /// <summary>
         /// Gets the sample of the offending script. This is a non standard field sent by Firefox. The default is the empty string.
         /// </summary>
         [DataMember(Name = "script-sample")]
-        public string ScriptSample { get; set; } = String.Empty;
+        public string ScriptSample
+        {
+            get { return _scriptSample ?? String.Empty; }
+            set { _scriptSample = value; }
+        }
     }
 }
