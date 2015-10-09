2c6a9bcd1fd8b7882f0ad432255c1a72cd4a4b71
klings <klingsen@gmail.com>
2015-10-09 22:25:14 +0200

    Removed sessionsecurity projects, they now live in a separate repo.

--- Source/NWebsec.SessionSecurity/SessionState/IHmacHelper.cs
+++
@@ -1,9 +0,0 @@
-﻿// Copyright (c) André N. Klingsheim. See License.txt in the project root for license information.
-
-namespace NWebsec.SessionSecurity.SessionState
-{
-    public interface IHmacHelper
-    {
-        byte[] CalculateMac(byte[] key, byte[] input);
-    }
-}
