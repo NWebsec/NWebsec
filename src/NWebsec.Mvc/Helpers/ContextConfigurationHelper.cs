0317e73d81800320713ace0bc90bf1984c175b43
klings <klingsen@gmail.com>
2015-11-24 22:07:10 +0100

    Sorted the config on context handling. #59

--- src/NWebsec.Mvc/Helpers/ContextConfigurationHelper.cs
+++ src/NWebsec.Mvc/Helpers/ContextConfigurationHelper.cs
@@ -10,86 +10,40 @@ namespace NWebsec.Mvc.Helpers
 {
     internal class ContextConfigurationHelper : IContextConfigurationHelper
     {
-        //TODO rename contexts, they're not contexts from OWIN anymore.
         public IXRobotsTagConfiguration GetXRobotsTagConfiguration(HttpContext context)
         {
-            var owinContext = context.GetNWebsecContext();
-            if (owinContext != null && owinContext.XRobotsTag != null)
-            {
-                return owinContext.XRobotsTag;
-            }
             return context.GetNWebsecContext().XRobotsTag;
         }
 
         public IXFrameOptionsConfiguration GetXFrameOptionsConfiguration(HttpContext context)
         {
-            var owinContext = context.GetNWebsecContext();
-            if (owinContext != null && owinContext.XFrameOptions != null)
-            {
-                return owinContext.XFrameOptions;
-            }
             return context.GetNWebsecContext().XFrameOptions;
         }
 
         public ISimpleBooleanConfiguration GetXContentTypeOptionsConfiguration(HttpContext context)
         {
-            var owinContext = context.GetNWebsecContext();
-            if (owinContext != null && owinContext.XContentTypeOptions != null)
-            {
-                return owinContext.XContentTypeOptions;
-            }
             return context.GetNWebsecContext().XContentTypeOptions;
         }
 
         public ISimpleBooleanConfiguration GetXDownloadOptionsConfiguration(HttpContext context)
         {
-            var owinContext = context.GetNWebsecContext();
-            if (owinContext != null && owinContext.XDownloadOptions != null)
-            {
-                return owinContext.XDownloadOptions;
-            }
             return context.GetNWebsecContext().XDownloadOptions;
         }
 
         public IXXssProtectionConfiguration GetXXssProtectionConfiguration(HttpContext context)
         {
-            var owinContext = context.GetNWebsecContext();
-            if (owinContext != null && owinContext.XXssProtection != null)
-            {
-                return owinContext.XXssProtection;
-            }
             return context.GetNWebsecContext().XXssProtection;
         }
 
         public ICspConfiguration GetCspConfiguration(HttpContext context, bool reportOnly)
         {
-            if (reportOnly)
-            {
-                return GetCspReportonlyConfiguration(context);
-            }
-
-            var owinContext = context.GetNWebsecContext();
-            if (owinContext != null && owinContext.Csp != null)
-            {
-                return owinContext.Csp;
-            }
-            return context.GetNWebsecContext().Csp;
+            return reportOnly ? context.GetNWebsecContext().CspReportOnly : context.GetNWebsecContext().Csp;
         }
 
-        private ICspConfiguration GetCspReportonlyConfiguration(HttpContext context)
+        public CspOverrideConfiguration GetCspConfigurationOverride(HttpContext context, bool reportOnly, bool allowNull)
         {
-            var owinContext = context.GetNWebsecContext();
-            if (owinContext != null && owinContext.CspReportOnly != null)
-            {
-                return owinContext.CspReportOnly;
-            }
-            return context.GetNWebsecContext().CspReportOnly;
-        }
-
-        public CspOverrideConfiguration GetCspConfigurationOverride(HttpContext httpContext, bool reportOnly, bool allowNull)
-        {
-            var context = httpContext.GetNWebsecContext() ?? httpContext.GetNWebsecContext();
-            var configOverride = GetConfigOverrides(context);
+            var nwContext = context.GetNWebsecContext();
+            var configOverride = GetConfigOverrides(nwContext);
 
             if (allowNull)
             {
--- test/NWebsec.Mvc.Tests/Helpers/ContextConfigurationHelperTests.cs
+++ test/NWebsec.Mvc.Tests/Helpers/ContextConfigurationHelperTests.cs
@@ -1,6 +1,5 @@
 ﻿// Copyright (c) André N. Klingsheim. See License.txt in the project root for license information.
 
-using System.Collections.Generic;
 using Microsoft.AspNet.Http;
 using Moq;
 using NUnit.Framework;
@@ -11,76 +10,41 @@ using NWebsec.Mvc.Helpers;
 
 namespace NWebsec.Mvc.Tests.Helpers
 {
-    [TestFixture, Ignore("Needs rewrite")]
+    [TestFixture]
     public class ContextConfigurationHelperTests
     {
-        private NWebsecContext _systemWebContext;
-        private NWebsecContext _owinContext;
+        private NWebsecContext _nwContext;
         private HttpContext _mockContext;
         private ContextConfigurationHelper _contextHelper;
 
         [SetUp]
         public void Setup()
         {
-            _systemWebContext = new NWebsecContext();
-            _owinContext = new NWebsecContext();
+            _nwContext = new NWebsecContext();
 
             var mockContext = new Mock<HttpContext>();
-            mockContext.Setup(c => c.Items["nwebsec.Context"]).Returns(_systemWebContext);
+            mockContext.Setup(c => c.Items["nwebsec.Context"]).Returns(_nwContext);
 
             _mockContext = mockContext.Object;
-
             _contextHelper = new ContextConfigurationHelper();
         }
 
-        private void SetupOwinContext()
-        {
-            var owinEnv = new Dictionary<string, object>();
-            owinEnv["nwebsec.Context"] = _owinContext;
-            Mock.Get(_mockContext).Setup(c => c.Items["owin.Environment"]).Returns(owinEnv);
-
-        }
-
         [Test]
-        public void GetXRobotsTagConfiguration_NoOwinContext_ReturnsSystemWebConfig()
+        public void GetXRobotsTagConfiguration_ReturnsContextConfig()
         {
             var config = new XRobotsTagConfiguration();
-            _systemWebContext.XRobotsTag = config;
+            _nwContext.XRobotsTag = config;
 
             var result = _contextHelper.GetXRobotsTagConfiguration(_mockContext);
 
             Assert.AreSame(config, result);
         }
 
-        [Test]
-        public void GetXRobotsTagConfiguration_OwinContextWithoutConfig_ReturnsSystemWebConfig()
-        {
-            SetupOwinContext();
-            var config = new XRobotsTagConfiguration();
-            _systemWebContext.XRobotsTag = config;
-
-            var result = _contextHelper.GetXRobotsTagConfiguration(_mockContext);
-
-            Assert.AreSame(config, result);
-        }
-
-        [Test]
-        public void GetXRobotsTagConfiguration_HasOwinConfig_ReturnsOwinConfig()
-        {
-            SetupOwinContext();
-            var config = new XRobotsTagConfiguration();
-            _owinContext.XRobotsTag = config;
-
-            var result = _contextHelper.GetXRobotsTagConfiguration(_mockContext);
-
-            Assert.AreSame(config, result);
-        }
-
-        [Test]
-        public void GetXFrameOptionsConfiguration_NoOwinContext_ReturnsSystemWebConfig()
+       [Test]
+        public void GetXFrameOptionsConfiguration_ReturnsContextConfig()
         {
             var config = new XFrameOptionsConfiguration();
-            _systemWebContext.XFrameOptions = config;
+            _nwContext.XFrameOptions = config;
 
             var result = _contextHelper.GetXFrameOptionsConfiguration(_mockContext);
 
@@ -88,34 +52,10 @@ namespace NWebsec.Mvc.Tests.Helpers
         }
 
         [Test]
-        public void GetXFrameOptionsConfiguration_OwinContextWithoutConfig_ReturnsSystemWebConfig()
-        {
-            SetupOwinContext();
-            var config = new XFrameOptionsConfiguration();
-            _systemWebContext.XFrameOptions = config;
-
-            var result = _contextHelper.GetXFrameOptionsConfiguration(_mockContext);
-
-            Assert.AreSame(config, result);
-        }
-
-        [Test]
-        public void GetXFrameOptionsConfiguration_HasOwinConfig_ReturnsOwinConfig()
-        {
-            SetupOwinContext();
-            var config = new XFrameOptionsConfiguration();
-            _owinContext.XFrameOptions = config;
-
-            var result = _contextHelper.GetXFrameOptionsConfiguration(_mockContext);
-
-            Assert.AreSame(config, result);
-        }
-
-        [Test]
-        public void GetXContentTypeOptionsConfiguration_NoOwinContext_ReturnsSystemWebConfig()
+        public void GetXContentTypeOptionsConfiguration_ReturnsContextConfig()
         {
             var config = new SimpleBooleanConfiguration();
-            _systemWebContext.XContentTypeOptions = config;
+            _nwContext.XContentTypeOptions = config;
 
             var result = _contextHelper.GetXContentTypeOptionsConfiguration(_mockContext);
 
@@ -123,34 +63,10 @@ namespace NWebsec.Mvc.Tests.Helpers
         }
 
         [Test]
-        public void GetXContentTypeOptionsConfiguration_OwinContextWithoutConfig_ReturnsSystemWebConfig()
-        {
-            SetupOwinContext();
-            var config = new SimpleBooleanConfiguration();
-            _systemWebContext.XContentTypeOptions = config;
-
-            var result = _contextHelper.GetXContentTypeOptionsConfiguration(_mockContext);
-
-            Assert.AreSame(config, result);
-        }
-
-        [Test]
-        public void GetXContentTypeOptionsConfiguration_HasOwinConfig_ReturnsOwinConfig()
-        {
-            SetupOwinContext();
-            var config = new SimpleBooleanConfiguration();
-            _owinContext.XContentTypeOptions = config;
-
-            var result = _contextHelper.GetXContentTypeOptionsConfiguration(_mockContext);
-
-            Assert.AreSame(config, result);
-        }
-
-        [Test]
-        public void GetXDownloadOptionsConfiguration_NoOwinContext_ReturnsSystemWebConfig()
+        public void GetXDownloadOptionsConfiguration_ReturnsContextConfig()
         {
             var config = new SimpleBooleanConfiguration();
-            _systemWebContext.XDownloadOptions = config;
+            _nwContext.XDownloadOptions = config;
 
             var result = _contextHelper.GetXDownloadOptionsConfiguration(_mockContext);
 
@@ -158,34 +74,10 @@ namespace NWebsec.Mvc.Tests.Helpers
         }
 
         [Test]
-        public void GetXDownloadOptionsConfiguration_OwinContextWithoutConfig_ReturnsSystemWebConfig()
-        {
-            SetupOwinContext();
-            var config = new SimpleBooleanConfiguration();
-            _systemWebContext.XDownloadOptions = config;
-
-            var result = _contextHelper.GetXDownloadOptionsConfiguration(_mockContext);
-
-            Assert.AreSame(config, result);
-        }
-
-        [Test]
-        public void GetXDownloadOptionsConfiguration_HasOwinConfig_ReturnsOwinConfig()
-        {
-            SetupOwinContext();
-            var config = new SimpleBooleanConfiguration();
-            _owinContext.XDownloadOptions = config;
-
-            var result = _contextHelper.GetXDownloadOptionsConfiguration(_mockContext);
-
-            Assert.AreSame(config, result);
-        }
-
-        [Test]
-        public void GetXXssProtectionConfiguration_NoOwinContext_ReturnsSystemWebConfig()
+        public void GetXXssProtectionConfiguration_ReturnsContextConfig()
         {
             var config = new XXssProtectionConfiguration();
-            _systemWebContext.XXssProtection = config;
+            _nwContext.XXssProtection = config;
 
             var result = _contextHelper.GetXXssProtectionConfiguration(_mockContext);
 
@@ -193,34 +85,10 @@ namespace NWebsec.Mvc.Tests.Helpers
         }
 
         [Test]
-        public void GetXXssProtectionConfiguration_OwinContextWithoutConfig_ReturnsSystemWebConfig()
-        {
-            SetupOwinContext();
-            var config = new XXssProtectionConfiguration();
-            _systemWebContext.XXssProtection = config;
-
-            var result = _contextHelper.GetXXssProtectionConfiguration(_mockContext);
-
-            Assert.AreSame(config, result);
-        }
-
-        [Test]
-        public void GetXXssProtectionConfiguration_HasOwinConfig_ReturnsOwinConfig()
-        {
-            SetupOwinContext();
-            var config = new XXssProtectionConfiguration();
-            _owinContext.XXssProtection = config;
-
-            var result = _contextHelper.GetXXssProtectionConfiguration(_mockContext);
-
-            Assert.AreSame(config, result);
-        }
-
-        [Test]
-        public void GetCspConfiguration_NoOwinContext_ReturnsSystemWebConfig()
+        public void GetCspConfiguration_ReturnsContextConfig()
         {
             var config = new CspConfiguration();
-            _systemWebContext.Csp = config;
+            _nwContext.Csp = config;
 
             var result = _contextHelper.GetCspConfiguration(_mockContext, false);
 
@@ -228,34 +96,10 @@ namespace NWebsec.Mvc.Tests.Helpers
         }
 
         [Test]
-        public void GetCspConfiguration_OwinContextWithoutConfig_ReturnsSystemWebConfig()
-        {
-            SetupOwinContext();
-            var config = new CspConfiguration();
-            _systemWebContext.Csp = config;
-
-            var result = _contextHelper.GetCspConfiguration(_mockContext, false);
-
-            Assert.AreSame(config, result);
-        }
-
-        [Test]
-        public void GetCspConfiguration_HasOwinConfig_ReturnsOwinConfig()
-        {
-            SetupOwinContext();
-            var config = new CspConfiguration();
-            _owinContext.Csp = config;
-
-            var result = _contextHelper.GetCspConfiguration(_mockContext, false);
-
-            Assert.AreSame(config, result);
-        }
-
-        [Test]
-        public void GetCspReportonlyConfiguration_NoOwinContext_ReturnsSystemWebConfig()
+        public void GetCspReportonlyConfiguration_ReturnsContextConfig()
         {
             var config = new CspConfiguration();
-            _systemWebContext.CspReportOnly = config;
+            _nwContext.CspReportOnly = config;
 
             var result = _contextHelper.GetCspConfiguration(_mockContext, true);
 
@@ -263,52 +107,21 @@ namespace NWebsec.Mvc.Tests.Helpers
         }
 
         [Test]
-        public void GetCspReportonlyConfiguration_OwinContextWithoutConfig_ReturnsSystemWebConfig()
+        public void GetCspConfigurationOverride_AllowNull_ReturnsNull([Values(true, false)] bool reportOnly)
         {
-            SetupOwinContext();
-            var config = new CspConfiguration();
-            _systemWebContext.CspReportOnly = config;
-
-            var result = _contextHelper.GetCspConfiguration(_mockContext, true);
-
-            Assert.AreSame(config, result);
-        }
-
-        [Test]
-        public void GetCspReportonlyConfiguration_HasOwinConfig_ReturnsOwinConfig()
-        {
-            SetupOwinContext();
-            var config = new CspConfiguration();
-            _owinContext.CspReportOnly = config;
-
-            var result = _contextHelper.GetCspConfiguration(_mockContext, true);
-
-            Assert.AreSame(config, result);
-        }
-
-        [Test]
-        public void GetCspConfigurationOverride_AllowNull_ReturnsNull()
-        {
-            var result = _contextHelper.GetCspConfigurationOverride(_mockContext, false, true);
+            var result = _contextHelper.GetCspConfigurationOverride(_mockContext, reportOnly, true);
 
             Assert.IsNull(result);
         }
 
-        [Test]
-        public void GetCspReportOnlyConfigurationOverride_AllowNull_ReturnsNull()
-        {
-            var result = _contextHelper.GetCspConfigurationOverride(_mockContext, true, true);
-
-            Assert.IsNull(result);
-        }
-
+        
         [Test]
         public void GetCspConfigurationOverride_NotAllowNull_ReturnsOverrideConfig()
         {
             var result = _contextHelper.GetCspConfigurationOverride(_mockContext, false, false);
 
             Assert.IsNotNull(result);
-            Assert.AreSame(_systemWebContext.ConfigOverrides.CspOverride, result);
+            Assert.AreSame(_nwContext.ConfigOverrides.CspOverride, result);
         }
 
         [Test]
@@ -317,7 +130,7 @@ namespace NWebsec.Mvc.Tests.Helpers
             var result = _contextHelper.GetCspConfigurationOverride(_mockContext, true, false);
 
             Assert.IsNotNull(result);
-            Assert.AreSame(_systemWebContext.ConfigOverrides.CspReportOnlyOverride, result);
+            Assert.AreSame(_nwContext.ConfigOverrides.CspReportOnlyOverride, result);
         }
 
         [Test]
@@ -326,7 +139,7 @@ namespace NWebsec.Mvc.Tests.Helpers
             var cspOverrideConfig = new CspOverrideConfiguration();
             var cspReportOnlyOverrideConfig = new CspOverrideConfiguration();
             var overrideConfig = new ConfigurationOverrides { CspOverride = cspOverrideConfig, CspReportOnlyOverride = cspReportOnlyOverrideConfig };
-            _systemWebContext.ConfigOverrides = overrideConfig;
+            _nwContext.ConfigOverrides = overrideConfig;
 
             var cspResult = _contextHelper.GetCspConfigurationOverride(_mockContext, false, allowNull);
             var cspReportOnlyResult = _contextHelper.GetCspConfigurationOverride(_mockContext, true, allowNull);
