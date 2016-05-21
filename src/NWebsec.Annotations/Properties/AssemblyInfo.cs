1bd61899cc68e1b2b7407a2f2f54e2634a3109a2
klings <klingsen@gmail.com>
2016-05-22 00:17:10 +0200

    Updated annotations project. #59

--- src/NWebsec.Annotations/Properties/AssemblyInfo.cs
+++ src/NWebsec.Annotations/Properties/AssemblyInfo.cs
@@ -5,14 +5,10 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("NWebsec.Annotations")]
-[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("NWebsec.Annotations")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
@@ -20,7 +16,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("144226e7-3eb3-459d-89c8-4d8720e1ef1e")]
-[assembly: InternalsVisibleTo("NWebsec.Core")]
-[assembly: InternalsVisibleTo("NWebsec.Middleware")]
-[assembly: InternalsVisibleTo("NWebsec.Mvc")]
+[assembly: Guid("d6357c78-1f5c-4e97-91ec-9822782b4c38")]
+[assembly: InternalsVisibleTo("NWebsec.AspNetCore.Core")]
+[assembly: InternalsVisibleTo("NWebsec.AspNetCore.Middleware")]
+[assembly: InternalsVisibleTo("NWebsec.AspNetCore.Mvc")]
