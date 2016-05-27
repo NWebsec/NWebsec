c982bec496d0d6d76fe322eaa34b0bb1c3d03641
klings <klingsen@gmail.com>
2016-05-27 22:41:14 +0200

    Trimmed AssemblyInfo's.

--- src/NWebsec.Annotations/Properties/AssemblyInfo.cs
+++ src/NWebsec.Annotations/Properties/AssemblyInfo.cs
@@ -10,13 +10,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("NWebsec.Annotations")]
 [assembly: AssemblyTrademark("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("d6357c78-1f5c-4e97-91ec-9822782b4c38")]
 [assembly: InternalsVisibleTo("NWebsec.AspNetCore.Core")]
 [assembly: InternalsVisibleTo("NWebsec.AspNetCore.Middleware")]
 [assembly: InternalsVisibleTo("NWebsec.AspNetCore.Mvc")]
