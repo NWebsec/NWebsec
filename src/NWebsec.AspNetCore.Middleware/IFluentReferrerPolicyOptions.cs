b98c7e9499f0543720c7500f721ebacf9cf376b0
klings <klingsen@gmail.com>
2017-03-26 17:06:41 +0200

    Minor tweak to docs on fluent options. #90

--- src/NWebsec.AspNetCore.Middleware/IFluentReferrerPolicyOptions.cs
+++ src/NWebsec.AspNetCore.Middleware/IFluentReferrerPolicyOptions.cs
@@ -10,50 +10,50 @@ namespace NWebsec.AspNetCore.Middleware
     public interface IFluentReferrerPolicyOptions : IFluentInterface
     {
         /// <summary>
-        /// Enables the no-referrer directive, instructing the browser to not send referrer information.
+        /// Enables the no-referrer policy, instructing the browser to not send referrer information.
         /// </summary>
         void NoReferrer();
 
         /// <summary>
-        /// Enables the no-referrer-when-downgrade directive, instructing the browser to send full referrer
+        /// Enables the no-referrer-when-downgrade policy, instructing the browser to send full referrer
         /// information unless navigation is from HTTPS->HTTP.
         /// </summary>
         void NoReferrerWhenDowngrade();
 
         /// <summary>
-        /// Enables the same-origin directive, instructing the browser to send full referrer information for
+        /// Enables the same-origin policy, instructing the browser to send full referrer information for
         /// same-origin requests and no referrer for cross-origin requests.
         /// </summary>
         void SameOrigin();
 
         /// <summary>
-        /// Enables the origin directive, instructing the browser to send origin (no path and query) as
+        /// Enables the origin policy, instructing the browser to send origin (no path and query) as
         /// referrer information for both same-origin and cross-origin requests.
         /// </summary>
         void Origin();
 
         /// <summary>
-        /// Enables the strict-origin directive, instructing the browser to send origin (no path and query) as
+        /// Enables the strict-origin policy, instructing the browser to send origin (no path and query) as
         /// referrer information for both same-origin and cross-origin HTTPS -> HTTPS and HTTP -> HTTP requests.
         /// HTTPS -> HTTP requests will not include referrer information.
         /// </summary>
         void StrictOrigin();
 
         /// <summary>
-        /// Enables the origin-when-cross-origin directive, instructing the browser to send full referrer information for
+        /// Enables the origin-when-cross-origin policy, instructing the browser to send full referrer information for
         /// same-origin requests and origin (no path and query) as referrer information for cross-origin requests (includes HTTPS -> HTTP and HTTP -> HTTPS).
         /// </summary>
         void OriginWhenCrossOrigin();
 
         /// <summary>
-        /// Enables the strict-origin-when-cross-origin directive, instructing the browser to send full referrer information for
+        /// Enables the strict-origin-when-cross-origin policy, instructing the browser to send full referrer information for
         /// same-origin requests and origin (no path and query) as referrer information for cross-origin requests. Referrer information
         /// is not sent for HTTPS -> HTTP requests.
         /// </summary>
         void StrictOriginWhenCrossOrigin();
 
         /// <summary>
-        /// Enables the unsafe-url directive, instructing the browser to send full referrer information for all requests.
+        /// Enables the unsafe-url policy, instructing the browser to send full referrer information for all requests.
         /// Note that this will leak full referrer information for HTTPS -> HTTP requests, which is even more unsafe than default browser behaviour.
         /// </summary>
         void UnsafeUrl();
