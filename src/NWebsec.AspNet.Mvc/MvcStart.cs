1d54df84af16762413e5d9ce467283132a8dfab6
klings <klingsen@gmail.com>
2018-03-10 23:27:37 +0100

    Reenabled PreApplicationStartMethod to disable MVC version header

--- src/NWebsec.AspNet.Mvc/MvcStart.cs
+++ src/NWebsec.AspNet.Mvc/MvcStart.cs
@@ -1,6 +1,10 @@
 ﻿// Copyright (c) André N. Klingsheim. See License.txt in the project root for license information.
 
+using System.Web;
 using System.Web.Mvc;
+using NWebsec.Mvc;
+
+[assembly: PreApplicationStartMethod(typeof(MvcStart), "DisableMvcVersionHeader")]
 
 namespace NWebsec.Mvc
 {
