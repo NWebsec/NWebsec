2e0c3826cc1f8876067cd9b0538492fb0022a323
klings <klingsen@gmail.com>
2018-03-11 00:09:12 +0100

    Added classic mvc app for easy testing.

---
+++ src/MvcClassic/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NWebsec.Mvc.HttpHeaders.Csp;
+
+namespace MvcClassic.Controllers
+{
+    [Csp, CspScriptSrc(Self = true, StrictDynamic = true)]
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
