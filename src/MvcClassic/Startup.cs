2334ad1fda644f54c619a662ce06fa109b45ece6
klings <klingsen@gmail.com>
2018-03-11 14:32:00 +0100

    Added Katana to testweb

---
+++ src/MvcClassic/Startup.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Owin;
+using NWebsec.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(MvcClassic.Startup))]
+
+namespace MvcClassic
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            //app.UseCsp(opts => opts
+            //    .DefaultSources(s => s.Self())
+            //.ScriptSources(s => s.Self().StrictDynamic()));
+        }
+    }
+}
