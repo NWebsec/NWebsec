d611d2300dcb9443cbd661d3ce860df10272b1ff
André N. Klingsheim <klingsen@gmail.com>
2020-01-12 15:55:57 +0100

    Tidy up project files

--- src/NWebsec.AspNetCore.Core/Properties/AssemblyInfo.cs
+++ src/NWebsec.AspNetCore.Core/Properties/AssemblyInfo.cs
@@ -1,13 +1,3 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("NWebsec.AspNetCore.Core")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+﻿using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("NWebsec.AspNetCore.Core.Tests")]
--- src/NWebsec.AspNetCore.Mvc/Properties/AssemblyInfo.cs
+++ src/NWebsec.AspNetCore.Mvc/Properties/AssemblyInfo.cs
@@ -1,14 +1,4 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("NWebsec.AspNetCore.Mvc")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+﻿using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("NWebsec.AspNetCore.Mvc.TagHelpers")]
 [assembly: InternalsVisibleTo("NWebsec.AspNetCore.Mvc.TagHelpers.Tests")]
